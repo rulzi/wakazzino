638bf4c627ac8c4d0e7014064fa6244233a35646
Khoirul Afandi <science.afandi@gmail.com>
2021-09-18 20:35:02 +0700

    fix user afandi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,12 +18,12 @@ class HomeController extends Controller
         $client = new Client(['base_uri' => 'https://panel.kazzino.net']);
 
         $data = [
-            'api_key' => "fa43f1d7-2a1f-11eb-991d-cab991d2ebb3",
+            'api_key' => "4c8b571d-1883-11ec-bd2a-cab991d2ebb3",
             'destination' => $request->number,
             'message' => $request->subject,
         ];
 
-        $send = $client->request('POST', '/api/whatsapp/instances/send-text/50022', ['body' => json_encode($data)]);
+        $send = $client->request('POST', '/api/whatsapp/instances/send-text/50023', ['body' => json_encode($data)]);
 
         return redirect(route('index'));
     }
